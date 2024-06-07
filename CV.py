8c431fff5d2d7a371c9f6b2d915e9f9673e67571
CatMaster <trabajo.nestor.098@gmail.com>
2024-06-07 11:39:45 +0200

    Actualizacion de Datos

--- CV.py
+++ CV.py
@@ -28,7 +28,9 @@ class Ventana():
         self.mail = ImageTk.PhotoImage(Image.open(f"{os.getcwd()}/Image/mail.png").resize((20,18)))
         self.cel = ImageTk.PhotoImage(Image.open(f"{os.getcwd()}/Image/cell.png").resize((20,18)))
         self.ubi = ImageTk.PhotoImage(Image.open(f"{os.getcwd()}/Image/ubicacion.png").resize((20,18)))
-        self.Aciz = ImageTk.PhotoImage(Image.open(f"{os.getcwd()}/Image/Muestra.png").resize((90,60)))
+        self.Aciz = ImageTk.PhotoImage(Image.open(f"{os.getcwd()}/Image/aciz.png").resize((90,60)))
+        self.scarpetoss = ImageTk.PhotoImage(Image.open(f"{os.getcwd()}/Image/scarpetoss.png").resize((105,97)))
+        self.eduplus = ImageTk.PhotoImage(Image.open(f"{os.getcwd()}/Image/eduplus.png").resize((70,80)))
         self.WhatsApp = ImageTk.PhotoImage(Image.open(f"{os.getcwd()}/Image/w.png").resize((35,35)))
         self.Twitter = ImageTk.PhotoImage(Image.open(f"{os.getcwd()}/Image/t.png").resize((35,35)))
         self.Instagram = ImageTk.PhotoImage(Image.open(f"{os.getcwd()}/Image/i.png").resize((39,39)))
@@ -62,7 +64,7 @@ class Ventana():
         Button(self.Frame_C, image=self.mail, bg='#303846', relief=FLAT, bd=0, activebackground='#303846', command=lambda:cp.copy('trabajo.nestor.098@gmail.com')).place(x=290, y=122)
 
         #Numero Telefonico
-        Button(self.Frame_C, text='+584144412054', font=('Arial', 11, 'bold'), bg='#303846', foreground='#979ba2', bd=0, relief=FLAT, activebackground='#303846', activeforeground='#FFFFFF', command=lambda:self.Browser(1)).place(x=610, y=119)
+        Button(self.Frame_C, text='+393888683169', font=('Arial', 11, 'bold'), bg='#303846', foreground='#979ba2', bd=0, relief=FLAT, activebackground='#303846', activeforeground='#FFFFFF', command=lambda:self.Browser(1)).place(x=610, y=119)
         Button(self.Frame_C, image=self.cel, bg='#303846', relief=FLAT, bd=0, activebackground='#303846', command=lambda:self.Browser(1)).place(x=588, y=122)
 
         #Ubicacion
@@ -79,21 +81,23 @@ class Ventana():
 
         #Primera competencia
         Label(self.Frame_A, text='Python', font=('Arial', 10, 'bold'), bg='#ECECEC').place(x=16, y=75)
-        Label(self.Frame_A,  bg='#303846').place(x=20, y=105, width=212, height=8)
+        Label(self.Frame_A,  bg='#BDBDBD').place(x=20, y=105, width=212, height=8)
+        Label(self.Frame_A,  bg='#303846').place(x=20, y=105, width=170, height=8)
         
         #Segunda Competencia
         Label(self.Frame_A, text='Git', font=('Arial', 10, 'bold'), bg='#ECECEC').place(x=16, y=140)
-        Label(self.Frame_A,  bg='#303846').place(x=20, y=170, width=212, height=8)
+        Label(self.Frame_A,  bg='#BDBDBD').place(x=20, y=170, width=212, height=8)
+        Label(self.Frame_A,  bg='#303846').place(x=20, y=170, width=150, height=8)
 
         #Tercera Competencia
-        Label(self.Frame_A, text='Django', font=('Arial', 10, 'bold'), bg='#ECECEC').place(x=16, y=205)
+        Label(self.Frame_A, text='Laravel', font=('Arial', 10, 'bold'), bg='#ECECEC').place(x=16, y=205)
         Label(self.Frame_A,  bg='#BDBDBD').place(x=20, y=235, width=212, height=8)
-        Label(self.Frame_A,  bg='#303846').place(x=20, y=235, width=150, height=8)
+        Label(self.Frame_A,  bg='#303846').place(x=20, y=235, width=140, height=8)
         
         #Cuarta Competencia
         Label(self.Frame_A, text='SQL', font=('Arial', 10, 'bold'), bg='#ECECEC').place(x=16, y=270)
         Label(self.Frame_A,  bg='#BDBDBD').place(x=20, y=300, width=212, height=8)
-        Label(self.Frame_A,  bg='#303846').place(x=20, y=300, width=180, height=8)
+        Label(self.Frame_A,  bg='#303846').place(x=20, y=300, width=122, height=8)
                 
         #Quinta Competencia
         Label(self.Frame_A, text='HTML', font=('Arial', 10, 'bold'), bg='#ECECEC').place(x=16, y=335)
@@ -104,7 +108,7 @@ class Ventana():
         #Sexta Competencia
         Label(self.Frame_A, text='CSS', font=('Arial', 10, 'bold'), bg='#ECECEC').place(x=16, y=400)
         Label(self.Frame_A,  bg='#BDBDBD').place(x=20, y=430, width=212, height=8)
-        Label(self.Frame_A,  bg='#303846').place(x=20, y=430, width=70, height=8)
+        Label(self.Frame_A,  bg='#303846').place(x=20, y=430, width=150, height=8)
 
 
         #=====================================Separacion===============================
@@ -120,7 +124,7 @@ class Ventana():
         #Italiano
         Label(self.Frame_A, text='Italiano', font=('Arial', 10, 'bold'), bg='#ECECEC').place(x=16, y=580)
         Label(self.Frame_A,  bg='#BDBDBD').place(x=20, y=610, width=212, height=8)
-        Label(self.Frame_A,  bg='#303846').place(x=20, y=610, width=130, height=8)
+        Label(self.Frame_A,  bg='#303846').place(x=20, y=610, width=100, height=8)
 
 #======================================================Menu Lateral 2=================================================
     def Aside_2(self):
@@ -129,11 +133,13 @@ class Ventana():
 
         #Septima competencia
         Label(self.Frame_A, text='JavaScripts', font=('Arial', 10, 'bold'), bg='#ECECEC').place(x=16, y=75)
-        Label(self.Frame_A,  bg='#303846').place(x=20, y=105, width=212, height=8)
+        Label(self.Frame_A,  bg='#BDBDBD').place(x=20, y=105, width=212, height=8)
+        Label(self.Frame_A,  bg='#303846').place(x=20, y=105, width=170, height=8)
         
         #Octava Competencia
-        Label(self.Frame_A, text='Tecnico Computo', font=('Arial', 10, 'bold'), bg='#ECECEC').place(x=16, y=140)
-        Label(self.Frame_A,  bg='#303846').place(x=20, y=170, width=212, height=8)
+        Label(self.Frame_A, text='Tailwind', font=('Arial', 10, 'bold'), bg='#ECECEC').place(x=16, y=140)
+        Label(self.Frame_A,  bg='#BDBDBD').place(x=20, y=170, width=212, height=8)
+        Label(self.Frame_A,  bg='#303846').place(x=20, y=170, width=135, height=8)
 
         #Novena Competencia
         Label(self.Frame_A, text='Diseño Grafico', font=('Arial', 10, 'bold'), bg='#ECECEC').place(x=16, y=205)
@@ -141,8 +147,9 @@ class Ventana():
         Label(self.Frame_A,  bg='#303846').place(x=20, y=235, width=150, height=8)
         
         #Decima Competencia
-        Label(self.Frame_A, text='WordPress', font=('Arial', 10, 'bold'), bg='#ECECEC').place(x=16, y=270)
-        Label(self.Frame_A,  bg='#303846').place(x=20, y=300, width=212, height=8)
+        Label(self.Frame_A, text='PHP', font=('Arial', 10, 'bold'), bg='#ECECEC').place(x=16, y=270)
+        Label(self.Frame_A,  bg='#BDBDBD').place(x=20, y=300, width=212, height=8)
+        Label(self.Frame_A,  bg='#303846').place(x=20, y=300, width=140, height=8)
                 
         #Onceava Competencia
         Label(self.Frame_A, text='WordPress | Elementor', font=('Arial', 10, 'bold'), bg='#ECECEC').place(x=16, y=335)
@@ -171,71 +178,85 @@ class Ventana():
         Label(self.Frame_M, bg='#C2C2C2').place(x=599, y=20, height=660, width=1)
 
         #Experiencias
-        Label(self.Frame_M, text='Experiencias', font=('Arial', 16, 'bold'), bg='#ECECEC').place(x=40, y=20)
+        Label(self.Frame_M, text='Esperienza', font=('Arial', 16, 'bold'), bg='#ECECEC').place(x=40, y=20)
 
 
         #Primera Experiencia
-        Label(self.Frame_M, text='Tecnico en computos', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=39, y=75)
-        Label(self.Frame_M, text='Mzo. 2018 - Presente', font=('Arial', 9), bg='#ECECEC').place(x=470, y=77)
+        Label(self.Frame_M, text='Tecnico informatico - CorporationTranslog', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=39, y=75)
+        Label(self.Frame_M, text='Gen. 2021 - Lug. 2022', font=('Arial', 9), bg='#ECECEC').place(x=470, y=77)
 
-        Label(self.Frame_M, text='Servicio Tecnico, Mexico', font=('Arial', 9), foreground='#767676', bg='#ECECEC').place(x=41, y=95)
+        Label(self.Frame_M, text='Servizio tecnico, Venezuela.', font=('Arial', 9), foreground='#767676', bg='#ECECEC').place(x=41, y=95)
 
         
-        Label(self.Frame_M, text='Atencion al clientes', font=('Arial', 9), bg='#ECECEC').place(x=55, y=115)
+        Label(self.Frame_M, text='Supporto tecnico / Consulenza ai clienti.', font=('Arial', 9), bg='#ECECEC').place(x=55, y=115)
         Label(self.Frame_M, text='•', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=45, y=113)
         
-        Label(self.Frame_M, text='Reparacion en equipos computos', font=('Arial', 9), bg='#ECECEC').place(x=55, y=135)
+        Label(self.Frame_M, text='Riparazione computer e server (Software e Hardware)..', font=('Arial', 9), bg='#ECECEC').place(x=55, y=135)
         Label(self.Frame_M, text='•', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=45, y=133)
 
-        Label(self.Frame_M, text='Optamizacion y analisis de sistemas computos', font=('Arial', 9), bg='#ECECEC').place(x=55, y=155)
+        Label(self.Frame_M, text='Assemblaggio (Hardware) computer e server.', font=('Arial', 9), bg='#ECECEC').place(x=55, y=155)
         Label(self.Frame_M, text='•', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=45, y=153)
 
-        Label(self.Frame_M, text='Soporte tecnico al cliente', font=('Arial', 9), bg='#ECECEC').place(x=55, y=175)
+        Label(self.Frame_M, text='Amministratore di zona.', font=('Arial', 9), bg='#ECECEC').place(x=55, y=175)
         Label(self.Frame_M, text='•', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=45, y=173)
+
+        Label(self.Frame_M, text='Sviluppo in Python, SQL e PostgreSQL.', font=('Arial', 9), bg='#ECECEC').place(x=55, y=195)
+        Label(self.Frame_M, text='•', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=45, y=192)
 
 
         #Segunda Experiencia
-        Label(self.Frame_M, text='Desarrollador de Aplicaciones de Escritorio', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=39, y=220)
-        Label(self.Frame_M, text='Dic. 2021 - Presente', font=('Arial', 9), bg='#ECECEC').place(x=470, y=222)
+        Label(self.Frame_M, text='Web Designer Full-Stack - qdqmedia', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=39, y=230)
+        Label(self.Frame_M, text='Nov. 2022 - Giu. 2023', font=('Arial', 9), bg='#ECECEC').place(x=470, y=232)
 
-        Label(self.Frame_M, text='Programador Full-Stack, Venezuela', font=('Arial', 9), foreground='#767676', bg='#ECECEC').place(x=40, y=240)
+        Label(self.Frame_M, text='Sviluppatore Web (Remoto), Spagna.', font=('Arial', 9), foreground='#767676', bg='#ECECEC').place(x=40, y=250)
 
         
-        Label(self.Frame_M, text='Analisis y Desarrollo de algoritmos', font=('Arial', 9), bg='#ECECEC').place(x=55, y=260)
-        Label(self.Frame_M, text='•', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=45, y=258)
+        Label(self.Frame_M, text='Web design.', font=('Arial', 9), bg='#ECECEC').place(x=55, y=270)
+        Label(self.Frame_M, text='•', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=45, y=268)
         
-        Label(self.Frame_M, text='Desarrollo en Python, SQLite, PostgreSQL', font=('Arial', 9), bg='#ECECEC').place(x=55, y=280)
-        Label(self.Frame_M, text='•', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=45, y=278)
+        Label(self.Frame_M, text='Sviluppo in JavaScript, HTML e CSS.', font=('Arial', 9), bg='#ECECEC').place(x=55, y=290)
+        Label(self.Frame_M, text='•', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=45, y=288)
 
-        Label(self.Frame_M, text='Diseñador Grafico', font=('Arial', 9), bg='#ECECEC').place(x=55, y=300)
-        Label(self.Frame_M, text='•', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=45, y=298)
+        Label(self.Frame_M, text='Creazione algoritmi.', font=('Arial', 9), bg='#ECECEC').place(x=55, y=310)
+        Label(self.Frame_M, text='•', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=45, y=308)
 
 
         #Tercera Experiencia
-        Label(self.Frame_M, text='Desarrollador de Paginas Webs', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=39, y=345)
-        Label(self.Frame_M, text='Sep. 2023 - Presente', font=('Arial', 9), bg='#ECECEC').place(x=470, y=347)
+        Label(self.Frame_M, text='Sviluppatore Web - BeeDigital', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=39, y=345)
+        Label(self.Frame_M, text='Gen. 2023 - Dic. 2023', font=('Arial', 9), bg='#ECECEC').place(x=470, y=347)
 
-        Label(self.Frame_M, text='Desarrollador Web Full-Stack, España', font=('Arial', 9), foreground='#767676', bg='#ECECEC').place(x=40, y=365)
+        Label(self.Frame_M, text='Sviluppatore Web (Remoto), Spagna.', font=('Arial', 9), foreground='#767676', bg='#ECECEC').place(x=40, y=365)
 
         
-        Label(self.Frame_M, text='Diseño Web', font=('Arial', 9), bg='#ECECEC').place(x=55, y=385)
+        Label(self.Frame_M, text='Web design.', font=('Arial', 9), bg='#ECECEC').place(x=55, y=385)
         Label(self.Frame_M, text='•', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=45, y=383)
         
-        Label(self.Frame_M, text='Desarrollo en JavaScript, HTML, CSS', font=('Arial', 9), bg='#ECECEC').place(x=55, y=405)
+        Label(self.Frame_M, text='Sviluppo in JavaScript, HTML, CSS, TypeScript e PHP.', font=('Arial', 9), bg='#ECECEC').place(x=55, y=405)
         Label(self.Frame_M, text='•', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=45, y=403)
 
-        Label(self.Frame_M, text='Creacion de algoritmos', font=('Arial', 9), bg='#ECECEC').place(x=55, y=425)
+        Label(self.Frame_M, text='Creazione algoritmi JavaScript.', font=('Arial', 9), bg='#ECECEC').place(x=55, y=425)
         Label(self.Frame_M, text='•', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=45, y=423)
 
-        Label(self.Frame_M, text='Creacion de paginas webs en WordPress | Elementor y Astra', font=('Arial', 9), bg='#ECECEC').place(x=55, y=445)
+        Label(self.Frame_M, text='Creazione web site in WordPress (Elementor e Astra).', font=('Arial', 9), bg='#ECECEC').place(x=55, y=445)
         Label(self.Frame_M, text='•', font=('Arial', 12, 'bold'), bg='#ECECEC').place(x=45, y=443)
         
-        #Aplicaciones
+        #Aplicaciones 1
         Label(self.Frame_M, text='Proyectos', font=('Arial', 16, 'bold'), bg='#ECECEC').place(x=40, y=485)
 
-        Button(self.Frame_M, image=self.Aciz, bg='#ECECEC', bd=0, relief=FLAT, activebackground='#ECECEC', command=lambda:self.Browser(7)).place(x=100, y=530)
-        Button(self.Frame_M, text='Programa de Gestion y Administracion', font=('Arial', 9), foreground='#767676', bg='#ECECEC', bd=0, relief=FLAT, activebackground='#ECECEC', command=lambda:self.Browser(7)).place(x=40, y=590)
-        Button(self.Frame_M, text='de negocios y comercios', font=('Arial', 9), foreground='#767676', bg='#ECECEC', bd=0, relief=FLAT, activebackground='#ECECEC', command=lambda:self.Browser(7)).place(x=75, y=610)
+        Button(self.Frame_M, image=self.Aciz, bg='#ECECEC', bd=0, relief=FLAT, activebackground='#ECECEC', command=lambda:self.Browser(7)).place(x=75, y=530)
+        Button(self.Frame_M, text='ACIZ', font=('Arial', 8), foreground='#767676', bg='#ECECEC', bd=0, relief=FLAT, activebackground='#ECECEC', command=lambda:self.Browser(7)).place(x=107, y=590)
+        Button(self.Frame_M, text='Programa de gestion de negocios.', font=('Arial', 8), foreground='#767676', bg='#ECECEC', bd=0, relief=FLAT, activebackground='#ECECEC', command=lambda:self.Browser(7)).place(x=40, y=605)
+
+        #Aplicaciones 2
+        Button(self.Frame_M, image=self.scarpetoss, bg='#ECECEC', bd=0, relief=FLAT, activebackground='#ECECEC', command=lambda:self.Browser(8)).place(x=240, y=505)
+        Button(self.Frame_M, text='Scarpetoss', font=('Arial', 8), foreground='#767676', bg='#ECECEC', bd=0, relief=FLAT, activebackground='#ECECEC', command=lambda:self.Browser(7)).place(x=265, y=590)
+        Button(self.Frame_M, text='Pagina de ventas de zapatos.', font=('Arial', 8), foreground='#767676', bg='#ECECEC', bd=0, relief=FLAT, activebackground='#ECECEC', command=lambda:self.Browser(7)).place(x=220, y=605)
+
+        #Aplicaciones 3
+        Button(self.Frame_M, image=self.eduplus, bg='#ECECEC', bd=0, relief=FLAT, activebackground='#ECECEC', command=lambda:self.Browser(9)).place(x=410, y=515)
+        Button(self.Frame_M, text='EduPlus', font=('Arial', 8), foreground='#767676', bg='#ECECEC', bd=0, relief=FLAT, activebackground='#ECECEC', command=lambda:self.Browser(7)).place(x=430, y=590)
+        Button(self.Frame_M, text='Pagina de gestion escolar.', font=('Arial', 8), foreground='#767676', bg='#ECECEC', bd=0, relief=FLAT, activebackground='#ECECEC', command=lambda:self.Browser(7)).place(x=390, y=605)
+
 
 #======================================================Pies de Pagina=================================================
     def Footer(self):
@@ -257,35 +278,41 @@ class Ventana():
 #======================================================Links==========================================================
     def Browser(self, control):
         if control == 1:
-            url = ['https://wa.me/584144412054']
+            url = 'https://wa.me/393888683169'
 
         elif control == 2:
-            url = ['https://www.instagram.com/nd_nestor_d/']
+            url = 'https://www.instagram.com/__catmaster__/'
 
         elif control == 3:
-            url = ['https://twitter.com/Ndnestor_098']
+            url = 'https://twitter.com/Ndnestor_098'
 
         elif control == 4:
-            url = ['https://www.facebook.com/Ndnestor098']
+            url = 'https://www.facebook.com/Ndnestor098'
 
         elif control == 5:
-            url = ['https://www.linkedin.com/in/nestor-daniel-salom-nunez/']
+            url = 'https://www.linkedin.com/in/nestor-daniel-salom-nunez/'
         
         elif control == 6:
-            url = ['https://github.com/Ndnestor098']
+            url = 'https://github.com/Ndnestor098'
 
         elif control == 7:
-            url = ['https://github.com/Ndnestor098/Software_ACIZ']
+            url = 'https://github.com/Ndnestor098/Software_ACIZ'
+
+        elif control == 8:
+            url = 'https://github.com/Ndnestor098/ScarpetossLaravel'
+
+        elif control == 9:
+            url = 'https://github.com/Ndnestor098/EduPlus'
 
         else:
-            url =['https://www.google.com/maps?sca_esv=592257846&output=search&q=montemarano+avellino&source=lnms&entry=mc']
+            url ='https://www.google.com/maps?sca_esv=592257846&output=search&q=montemarano+avellino&source=lnms&entry=mc'
 
 
 
 
 
         
-        webbrowser.open_new(url[0])
+        webbrowser.open_new(url)
 
         
 if __name__ == '__main__':
